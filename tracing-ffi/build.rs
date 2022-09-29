b1bcd1c4ad82c7adcdaf7e9a6ffed21d1e45008c
jadamcrain <jadamcrain@gmail.com>
2022-09-29 13:16:23 -0700

    add the tracing implementation and a test build for it

---
+++ tracing-ffi/build.rs
@@ -0,0 +1,62 @@
+use oo_bindgen::model::*;
+use std::env;
+use std::io::Write;
+use std::path::Path;
+
+fn main() {
+    println!("cargo:rerun-if-changed=build.rs");
+
+    let mut file = std::fs::File::create(
+        Path::new(&env::var_os("OUT_DIR").unwrap()).join("implementation.rs"),
+    )
+    .unwrap();
+    file.write_all(tracing_ffi_schema::get_impl_file().as_bytes())
+        .unwrap();
+
+    let settings = LibrarySettings::create(
+        "tracing_ffi",
+        "tracing_ffi",
+        ClassSettings::default(),
+        IteratorSettings::default(),
+        CollectionSettings::default(),
+        FutureSettings::default(),
+        InterfaceSettings::default(),
+    )
+    .unwrap();
+
+    let info = LibraryInfo {
+        description: "just a test".to_string(),
+        project_url: "".to_string(),
+        repository: "".to_string(),
+        license_name: "".to_string(),
+        license_description: vec![],
+        license_path: Default::default(),
+        developers: vec![],
+        logo_png: &[],
+    };
+
+    let mut builder = oo_bindgen::model::LibraryBuilder::new(Version::new(0, 1, 0), info, settings);
+
+    let error_type = builder
+        .define_error_type(
+            "init_error",
+            "init_exception",
+            ExceptionType::UncheckedException,
+        )
+        .unwrap()
+        .add_error(
+            "tracing_init_failed",
+            "Unable to initialize tracing backend",
+        )
+        .unwrap()
+        .doc("error type")
+        .unwrap()
+        .build()
+        .unwrap();
+
+    tracing_ffi_schema::define(&mut builder, error_type).unwrap();
+
+    let lib = builder.build().unwrap();
+
+    rust_oo_bindgen::RustCodegen::new(&lib).generate().unwrap();
+}
---
+++ tracing-ffi/src/lib.rs
@@ -0,0 +1,18 @@
+pub mod ffi;
+mod implementation;
+
+pub(crate) use implementation::*;
+
+lazy_static::lazy_static! {
+    static ref VERSION: std::ffi::CString = std::ffi::CString::new("0.1.0").unwrap();
+}
+
+fn version() -> &'static std::ffi::CStr {
+    &VERSION
+}
+
+impl From<crate::TracingInitError> for std::os::raw::c_int {
+    fn from(_: crate::TracingInitError) -> Self {
+        ffi::InitError::TracingInitFailed.into()
+    }
+}
